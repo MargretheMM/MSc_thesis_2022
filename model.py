93047105000b442d51967b5638d402f500d9d096
MargretheMM <63294890+MargretheMM@users.noreply.github.com>
2022-04-05 13:41:18 +0200

    most of model adjusted, need to work on the growth rate part

--- model.py
+++ model.py
@@ -16,24 +16,30 @@ total_ribos = 190000
 # maximum number of ribosomes available for protein production - currently assume 80% of total
 max_avail_ribos = total_ribos*0.8
 
+#initally, most ribosomes are available
+free_ribos_init = max_avail_ribos*0.9
 
 # Maximum transcription and translation rates
 # transcription in codons per hour per cell : codons/sec * sec/hour * number of polymerases that can work at same time = codons/hour
-transcription_rate = 40 * 3600
-# translation in aas per hour per cell : aa/ribosome/sec * sec/hour * number of ribsomes which can be diverted = aa/hour
-translation_rate = 10 * 3600 * free_ribos
+transcription_rate = 40 * 3600 * 3
+# maximum translation in aas per hour per cell : aa/ribosome/sec * sec/hour * number of ribsomes which can be diverted = aa/hour
+translation_rate = 10 * 3600 * max_avail_ribos
 
+# max rate of production of new ribosomes : max rate per minute * min/hour =  max ribos per hour
+max_ribos_manufacturing_rate = 2000 * 60
 
-# Set degradation rates and maximum production rates for mRNA and protein
+# Set degradation rates and maximum production rates for mRNA and protein and ribosome (still needs checking)
 # rates based on half-life in h, ^-1
 deg_rate_m = 2
 deg_rate_p = 0.2
+deg_rate_ribos = 0.15
 
 # mRNA production rates - mix of copy number, promotor strength etc - will be scaled with transcription rate
 production_m_v, production_m_r = 100, 5
 
 # initial cell growth rate - in generations per hour - use as fitness parameter, if falls too low -> collapse
-dilution_rate = 0.3
+dilution_rate_init = 0.3
+max_dilution_rate = 0.7
 
 # contcentration constants - association/disassociation in one - need some ideas for this
 K_p_v = 1000000
@@ -43,25 +49,13 @@ K_stock = max_avail_ribos / 2
 
 
 # Control functions. Currently Michaelis-menten-like (Hill functions, so far n = 1)
-def control_positive(substrate_conc, K):
-    return substrate_conc/(K+substrate_conc)
+def control_positive(K, substrate):
+    return substrate/(K+substrate)
 
-def control_negative(substrate_conc, K):
-    return K/(K+substrate_conc)
+def control_negative(K, substrate):
+    return K/(K+substrate)
 
-# define stopping conditions
-# if run out of resources or get negative values of protein
-def run_out_store(t, y):
-    return y[4]
-run_out_store.terminal = True 
 
-def run_out_p_v(t, y):
-    return y[1]
-run_out_p_v.terminal = True 
-
-def run_out_p_r(t, y):
-    return y[3]
-run_out_p_r.terminal = True 
 
 # write up equations
 def model(indep, init_deps):
@@ -70,26 +64,26 @@ def model(indep, init_deps):
     Input: independent variable, initial values of dependent variables (list)
     Output: list of equations in the system 
     '''
-    m_v, p_v, m_r, p_r, store = init_deps
+    m_v, p_v, m_r, p_r, ribos, dilution = init_deps
 
     # equations
-    dm_v = control_negative(p_r, K_p_r) * production_m_v - deg_rate_m * m_v
-    dp_v = max_protein_production * control_positive(store, K_store) * m_v - deg_rate_p * p_v
-    dm_r = control_positive(p_v, K_p_v) * production_m_r - deg_rate_m * m_r
-    dp_r = max_protein_production * control_positive(store, K_store) * m_r - deg_rate_p * p_r
-    # assume constant resource consumption for protein for now
-    dstore = store_replenish - store_consumption * p_v - store_dilution
+    dm_v = control_negative(K_p_r, p_r) * production_m_v * transcription_rate - deg_rate_m * m_v
+    dp_v = translation_rate * control_positive(K_stock, ribos) * m_v - deg_rate_p * p_v - dilution_rate * p_v
+    dm_r = control_positive(K_p_v,p_v) * production_m_r * transcription_rate - deg_rate_m * m_r
+    dp_r = translation_rate * control_positive(K_stock, ribos) * m_r - deg_rate_p * p_r - dilution_rate * p_r
+    dribos = max_ribos_manufacturing_rate * control_positive(max_dilution_rate/2, dilution) - deg_rate_ribos * ribos - dilution * ribosome
+    if dilution > 0 and ribos < 
 
-    return [dm_v, dp_v, dm_r, dp_r, dstore]
+    return [dm_v, dp_v, dm_r, dp_r, dribos, ddilution]
 
 # list of initial conditions
-inits = [m_v_init, p_v_init, m_r_init, p_r_init, store_init]
+inits = [m_v_init, p_v_init, m_r_init, p_r_init, free_ribos_init, dilution_rate_init]
 
 # time steps
-t = np.linspace(0, 20, 100)
+t = np.linspace(0, 200, 1000)
 
 # solving the ODE system
-solution = ig.solve_ivp(model, [min(t),max(t)], inits, t_eval = t, events = [run_out_store, run_out_p_v,run_out_p_r])
+solution = ig.solve_ivp(model, [min(t),max(t)], inits, t_eval = t)
 plt.figure(1)
 plt.subplot(511)
 plt.plot(solution.t, solution.y[1].T)
@@ -105,6 +99,6 @@ plt.plot(solution.t, solution.y[3].T)
 plt.title('regulator protein')
 plt.subplot(515)
 plt.plot(solution.t, solution.y[4].T)
-plt.title('store level')
+plt.title('free ribosomes')
 #plt.legend(['m1', 'm2', 'p2', 'fitness index'])
 plt.show()

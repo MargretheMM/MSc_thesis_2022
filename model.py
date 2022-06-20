22dcf516d9e0e0c0dd7d48327c6a8b2427fbc8ff
MargretheMM <63294890+MargretheMM@users.noreply.github.com>
2022-06-20 11:45:56 +0200

    new equations, re-intro growth rate, also found issue regarding solution space

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import numpy as np
 ''' Model parameters - biologically based '''
 
 # maximum number of ribosomes available for protein production - currently assume 95% of total - Metzl-Raz 2017 and von der Haar 2008
-R= 2e5*0.95
+R_max= 2e5*0.95
 
 
 # total number of RNA polymerases in cell - Borggrefe 2001
@@ -17,6 +17,8 @@ total_pol = 30000
 
 # Maximum number of protein bound amino acids in cell - futcher et al 1999
 p_max = 3e10
+# Total codons of mRNA in cell - estimate from Siewiak 2010 - take high esitmate
+m_tot = 3e7
 
 # Maximum transcription and translation rates
 # max transcription in codons per hour per cell : codons/sec * sec/hour *number of polymerases = codons/hour - Yu and Nielsen 2019
@@ -25,19 +27,27 @@ beta_m = 50/3 * 3600 * total_pol
 beta_p = 10 * 3600
 
 
+# max rate of production of new ribosomes : max rate per minute * min/hour =  max s per hour - Warner 1999
+beta_R = 2000 * 60
+
 # Set degradation rates and maximum production rates for mRNA and protein - mRNA: Curran et al 2013, Perez-Ortin et al 2007, protein Christiano 2020
 # rates based on half-life in h
 alpha_m_v = np.log(2)/0.33
 alpha_p_v = np.log(2)/5
+alpha_R = np.log(2)/6
 
+#Maximum growth rate (based on doubling time of 80 minutes)
+k=np.log(2)*3/4
 
 ''' Presumed engineerable paramters '''
 # contcentration constants - association/disassociation in one - need some ideas for this
 K_p_v = 1e6
 K_p_r = 50
+K_m_v = 1e2
+K_R = 0.5 * R_max
 
 # mRNA production rates - mix of copy number, promotor strength etc - will be scaled with transcription rate
-delta_m_v, delta_m_r = 1, 0.002
+delta_m_v, delta_m_r = 0.2, 0
 
 # regulator degradation rates
 alpha_m_r = np.log(2)/0.33
@@ -47,20 +57,25 @@ alpha_p_r = np.log(2)/1
 # initial amounts of each product (mRNA and protein from each gene)
 m_v_init = 5
 p_v_init = 100
-m_r_init = 1
+m_r_init = 0
 p_r_init = 0
+R_init = R_max *0.99
 R_v_init = 5
-R_o_init = R-R_v_init
+R_o_init = R_init - R_v_init
 
 
 '''Functions and model '''
-# Control functions. Currently Michaelis-menten-like (Hill functions, so far n = 1)
+# Control functions -  Michaelis-menten-like
 def control_positive(K, substrate):
     return substrate/(K+substrate)
 
 def control_negative(K, substrate):
     return K/(K+substrate)
 
+# tracking if growth stops (R_o hits zero)
+#def no_growth(t,y): return y[6]
+# stop simulation if this happens
+#no_growth.terminal = True
 
 # write up equations
 def model(indep: float, init_deps):
@@ -69,53 +84,59 @@ def model(indep: float, init_deps):
     Input: independent variable, initial values of dependent variables (list)
     Output: list of equations in the system 
     '''
-    m_v, p_v, m_r, p_r, R_v, R_o = init_deps
+    m_v, p_v, m_r, p_r, R, R_v, R_o = init_deps
     
     # protein level in model
     p_tot = p_v + p_r + R * 12e3
     
-    # cost function
-    C = p_max*p_v/(p_max-p_v)
+    # current growth rate
+    gamma = k * R_o / R
     
     # equations
-    dm_v = beta_m * delta_m_v *  control_negative(K_p_r, p_r)  - alpha_m_v * m_v
-    
+    if (0 < m_v < m_tot):
+        dm_v = beta_m * delta_m_v *  control_negative(K_p_r, p_r)  - alpha_m_v * m_v
+    else:
+        dm_v = 0
+            
     if (0 < p_tot < p_max):
-        dp_v = beta_p * R_v - alpha_p_v * p_v
+        dp_v = beta_p * control_positive(K_R,R) * control_positive(K_m_v,m_v) - (alpha_p_v + gamma) * p_v
     else:
         dp_v = 0
 
     dm_r = beta_m * delta_m_r * control_positive(K_p_v,p_v) - alpha_m_r * m_r
     
     if (0 < p_tot < p_max):
-        dp_r = beta_p * R_o * m_r/(5e7-m_v)  - alpha_p_r * p_r
-        # 5e7 estimate of total amount of mRNA in cell
+        dp_r = beta_p * R_o * m_r/(m_tot - m_v)  - (alpha_p_r + gamma) * p_r
+
     else:
         dp_r = 0
   
-    if (0 < R_v < R):
-        dR_v = beta_p ** -1 * (dm_v * C + m_v * dp_v * p_max ** 2 / (p_max-p_v) ** 2 ) 
-    else:
-        dR_v = 0
+    if (0 < R < R_max):
+        dR = beta_R * R_o / R_max - (alpha_R + gamma) * R
+    else:        dR = 0
     
-    dR_o = - dR_v
-          
-    return [dm_v, dp_v, dm_r, dp_r, dR_v, dR_o]
+    dR_v = m_tot ** -1 * (dR * m_v + R * dm_v)
+    
+    dR_o = dR - dR_v
+#    print(p_v)
+    return [dm_v, dp_v, dm_r, dp_r, dR, dR_v, dR_o]
 
 # list of initial conditions
-inits = [m_v_init, p_v_init, m_r_init, p_r_init, R_v_init, R_o_init]
+inits = [m_v_init, p_v_init, m_r_init, p_r_init, R_init, R_v_init, R_o_init]
 
 # time steps
 t = np.linspace(0, 200, 1000)
 
 # solving the ODE system
 solution = ig.solve_ivp(model, [min(t),max(t)], inits, t_eval = t)
+#, events=no_growth)
 fig, axs = plt.subplot_mosaic([['mv', 'pv_vs_mv'],
                                ['pv', 'pv_vs_mv'],
                                ['mr', 'pr_vs_pv'],
                                ['pr', 'pr_vs_pv'],
-                               ['fr', 'fr_vs_pv'],
-                               ['gr', 'fr_vs_pv']])
+                               ['R', 'R_vs_pv'],
+                               ['Rv', 'R_vs_pv'],
+                               ['Ro', 'R_vs_pv']])
 axs['mv'].plot(solution.t, solution.y[0].T)
 axs['mv'].set_title('mRNA of gene of value')
 axs['pv'].plot(solution.t, solution.y[1].T)
@@ -127,19 +148,22 @@ axs['mr'].sharex(axs['mv'])
 axs['pr'].plot(solution.t, solution.y[3].T)
 axs['pr'].set_title('Regulator protein')
 axs['pr'].sharex(axs['mv'])
-axs['fr'].plot(solution.t, solution.y[4].T)
-axs['fr'].set_title('Value productive ribosomes')
-axs['fr'].sharex(axs['mv'])
-axs['gr'].plot(solution.t, solution.y[5].T)
-axs['gr'].set_title('Other ribosomes')
-axs['gr'].sharex(axs['mv'])
-axs['gr'].set_xlabel('Time in hours')
+axs['R'].plot(solution.t, solution.y[4].T)
+axs['R'].set_title('Ribosomes')
+axs['R'].sharex(axs['mv'])
+axs['Rv'].plot(solution.t, solution.y[5].T)
+axs['Rv'].set_title('Value productive ribosomes')
+axs['Rv'].sharex(axs['mv'])
+axs['Ro'].plot(solution.t, solution.y[6].T)
+axs['Ro'].set_title('Other ribosomes')
+axs['Ro'].sharex(axs['mv'])
+axs['Ro'].set_xlabel('Time in hours')
 axs['pv_vs_mv'].plot(solution.y[0].T, solution.y[1].T)
 axs['pv_vs_mv'].set_title('state space prot val vs mRNA val')
 axs['pr_vs_pv'].plot(solution.y[1].T, solution.y[3].T)
 axs['pr_vs_pv'].set_title('state space reg prot vs prot val')
-axs['fr_vs_pv'].plot(solution.y[1].T, solution.y[4].T)
-axs['fr_vs_pv'].set_title('state space free ribosomes vs prot val')
+axs['R_vs_pv'].plot(solution.y[1].T, solution.y[4].T)
+axs['R_vs_pv'].set_title('ribosomes vs prot val')
 
 
 plt.subplots_adjust(hspace=0.7)

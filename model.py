ee7acc12ee8a73b22ae20e9341e3d2d4e964b1b1
MargretheMM <63294890+MargretheMM@users.noreply.github.com>
2022-07-01 18:13:22 +0200

    rebase to seconds and thousands of codons rather than single codons

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ import numpy as np
 ENSURE_POSITIVE = False
 ''' Model parameters - biologically based '''
 # time base - the base timestep for the various constants in seconds
-t_base = 60
+t_base = 1
 
 # maximum number of ribosomes available for protein production - currently assume 75% of total - Metzl-Raz 2017 and von der Haar 2008
 R_max= 2e5 * 0.75
@@ -21,20 +21,20 @@ R_max= 2e5 * 0.75
 # total_pol = 30000
 
 # Maximum number of protein bound amino acids in cell - futcher et al 1999
-p_max = 3e10
+p_max = 3e7
 # Total codons of mRNA in cell - estimate from Siewiak 2010 - take high esitmate
-m_tot = 3e7
+m_tot = 3e4
 
 # Maximum transcription and translation rates
-# max transcription in codons per polymerase * 50 (assumed number of polymerases which can work on same bit of DNA) = codons/sec Yu and Nielsen 2019
-beta_m = 50/3 * t_base
+# max transcription in codons per polymerase * 10 (assumed number of polymerases which can work on same bit of DNA) = codons/sec Yu and Nielsen 2019
+beta_m = 50/300 * t_base
 # alternative way is calculating max rate similarly to max ribosome production rate, but using half-life: 3e7/2 codons should be produced in 15 minutes, which is about 17 000 codons per second - but this is cell wide -
 # maximum translation in aas per ribosome : aa/ribosome/sec
-beta_p = 10 * t_base
+beta_p = 0.01 * t_base
 
 
 # max rate of production of new ribosomes : max rate per second  =  max ribos per minute / seconds per minute - Warner 1999
-beta_R = 2500 / 60 * t_base 
+beta_R = 2500 / 60 * t_base
 
 # Set degradation rates and maximum production rates for mRNA and protein - mRNA: Curran et al 2013, Perez-Ortin et al 2007, protein Christiano 2020
 # rates based on half-life in minutes
@@ -47,9 +47,9 @@ k=np.log(2)/4800 * t_base
 
 ''' Presumed engineerable paramters '''
 # contcentration constants - association/disassociation in one - need some ideas for this
-K_p_v = 1e9
-K_p_r = 5000
-K_m_v = 1e5
+K_p_v = 1e6
+K_p_r = 2
+K_m_v = 1e2
 K_R = 0.5 * R_max
 
 # mRNA production rates - mix of copy number, promotor strength etc - scales transcription rate
@@ -63,8 +63,8 @@ alpha_p_r = alpha_p_v * multi_p_r
 
 ''' Initial conditions'''
 # initial amounts of each product (mRNA and protein from each gene)
-m_v_init = 5
-p_v_init = 100
+m_v_init = 0.005
+p_v_init = 0.1
 m_r_init = 0
 p_r_init = 0
 R_init = R_max * 0.99
@@ -106,7 +106,7 @@ def model(indep: float, init_deps):
     '''
     Defining the set of differential equations to be solved
     Input: independent variable, initial values of dependent variables (list)
-    Output: list of equations in the system 
+    Output: list of equations in the system
     '''
     global step_number
     step_number += 1
@@ -127,11 +127,11 @@ def model(indep: float, init_deps):
             assert thing >= -1e-3, thing
 
     # protein level in model
-    p_tot = p_v + p_r + R * 12e3
-    
+    p_tot = p_v + p_r + R * 12
+
     # current growth rate
     gamma = k * R_o / R
-    
+
     # equations
     if (0 < m_v < m_tot):
         dm_v = beta_m * delta_m_v * control_negative(K_p_r, p_r) - alpha_m_v * m_v
@@ -148,14 +148,14 @@ def model(indep: float, init_deps):
         dp_v = 0
 
     dm_r = beta_m * delta_m_r * control_positive(K_p_v,p_v) - alpha_m_r * m_r
-    
+
     if (0 < p_tot < p_max):
         dp_r = beta_p * R_o * m_r/(m_tot - m_v)  - (alpha_p_r + gamma) * p_r
     elif p_tot >= p_max:
         dp_r =  -(alpha_p_r + gamma) * p_r
     else:
         dp_r = 0
-  
+
     if (0 < R < R_max):
         dR = beta_R * R_o / R_max - (alpha_R + gamma) * R
     elif R >= R_max:

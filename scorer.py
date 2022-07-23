024b41da2b68d6eca41113ba86062c88509b811f
MargretheMM <63294890+MargretheMM@users.noreply.github.com>
2022-07-23 17:58:13 +0200

    added file with version numbers of libraries and file with scoring script

---
+++ scorer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import glob
+
+
+def score(prot, time):
+    return float(prot) / float(time)
+
+
+files = glob.glob("data_*.txt")
+
+values_by_filename ={}
+results = []
+for filename in files:
+    with open(filename) as handle:
+        lines = handle.read().splitlines()
+        _, proteins, time = lines[0].strip().split()
+        parameters = lines[2:]
+    results.append([filename, proteins, time, score(proteins, time)])
+    values_by_filename[filename] = parameters
+    
+results.sort(key=lambda x: x[3], reverse=True)
+for result in results[:5]:
+    print(result, values_by_filename[result[0]])

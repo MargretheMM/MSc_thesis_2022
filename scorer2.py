d7ca09663b52cc17d3134865168edd08a71e337f
MargretheMM <63294890+MargretheMM@users.noreply.github.com>
2022-07-29 18:59:28 +0200

    change scorer to save to csv

--- scorer2.py
+++ scorer2.py
@@ -9,16 +9,21 @@ def score(prot, time):
 
 files = glob.glob("data_*.txt")
 
-values_by_filename ={}
 results = []
 for filename in files:
     with open(filename) as handle:
+        file_result =[]
         lines = handle.read().splitlines()
         _, proteins, time = lines[0].strip().split()
+        file_result = [filename, proteins, time, str(score(proteins, time))]
         parameters = lines[2:]
-    results.append([filename, proteins, time, score(proteins, time)])
-    values_by_filename[filename] = parameters
-    
+        file_result.extend(parameters)
+    results.append(file_result)
+
 results.sort(key=lambda x: x[3], reverse=True)
-for result in results[:20]:
-    print(result, values_by_filename[result[0]])
+for result in results[:30]:
+    print(result)
+
+with open("results.csv",'w') as handle:
+    for result in results[:30]:
+        handle.write(";".join(result)+"\n")

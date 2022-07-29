115d190da3303cb8bab6fa09d81ec5ea4486e7d2
MargretheMM <63294890+MargretheMM@users.noreply.github.com>
2022-07-29 19:04:32 +0200

    updated scorer to save json

--- scorer2.py
+++ scorer2.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 
 import glob
-
+import json
 
 def score(prot, time):
     return float(prot) / float(time) ** 2
-
 
 files = glob.glob("data_*.txt")
 
 results = []
 for filename in files:
     with open(filename) as handle:
-        file_result =[]
+        file_result ={}
         lines = handle.read().splitlines()
-        _, proteins, time = lines[0].strip().split()
-        file_result = [filename, proteins, time, str(score(proteins, time))]
-        parameters = lines[2:]
-        file_result.extend(parameters)
-    results.append(file_result)
+        _, file_result["p_v"], file_result["T2"] = lines[0].strip().split()
+        file_result["score"] = score(file_result["p_v"], file_result["T2"])
+        linenum = 2
+        while linenum < len(lines):
+            param_pairs = lines[linenum].split(":")
+            file_result[param_pairs[0]] = float(param_pairs[1])
+            linenum += 1
+    if file_result not in results:
+        results.append(file_result)
 
-results.sort(key=lambda x: x[3], reverse=True)
-for result in results[:30]:
-    print(result)
+results.sort(key=lambda d: d["score"], reverse=True)
 
-with open("results.csv",'w') as handle:
-    for result in results[:30]:
-        handle.write(";".join(result)+"\n")
+with open("top20scores.json", 'w') as handle:
+    json.dump(results[:20],handle)
